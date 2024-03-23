d1374bd26b3c74992f96abf03573767fab2a88f3
kishanV2123 <kv230801@gmail.com>
2024-03-23 19:28:07 +0530

    name change in postcard

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import dbService from '../appwrite/config'
 import { Link } from 'react-router-dom'
-import { useSelector } from "react-redux";
 
 function PostCard({ $id, title, featuredImage }) {
-    const userData = useSelector((state) => state.auth.userData);
 
     return (
 
@@ -28,7 +26,7 @@ function PostCard({ $id, title, featuredImage }) {
                     <a href="#">
                         <h5 class="mb-2 text-xs sm:text-sm md:text-lg lg:text-xl xl:text-2xl font-bold text-wrap text-gray-900">{title}</h5>
                     </a>
-                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{userData.name}</p>
+                    {/* <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{userData.name}</p> */}
                     <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
                         Read more
                         <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
